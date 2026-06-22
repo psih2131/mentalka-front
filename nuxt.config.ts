ee20844db5313c03e0b9bc172ccb165e3c804335
psih2131 <gearsdevelope@gmail.com>
2026-06-22 11:22:15 +0300

    add reviews page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,5 +42,10 @@ export default defineNuxtConfig({
     },
   },
 
+  runtimeConfig: {
+    public: {
+      strapiUrl: process.env.NUXT_PUBLIC_STRAPI_URL || 'http://localhost:1337',
+    },
+  },
 
 })
